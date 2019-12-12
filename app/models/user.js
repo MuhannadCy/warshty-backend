86477f8c88d5278d2ce097005251a25c7e41022b
Muhannad Alanazi <muhannad.h.alanazi@gmail.com>
2019-12-12 16:01:49 +0300

    created route files and model files

--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  email: { type: String, required: true, unique: true },
   hashedPassword: {
     type: String,
-    required: true
-  },
+    required: true },
   token: String
-}, {
-  timestamps: true,
-  toObject: {
+}, {timestamps: true, toObject: {
     // remove `hashedPassword` field when we call `.toObject`
     transform: (_doc, user) => {
       delete user.hashedPassword
