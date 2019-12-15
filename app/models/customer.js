66657958c141baddc6a9825534a0db18b7404ae3
Abdulrhman-J <a.jammaza@gmail.com>
2019-12-15 16:09:17 +0300

    solve conflict

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
-//Abdulwahhab
-=======
 //Abdulwahhab
 const mongoose = require('mongoose')
 
 const exampleSchema = new mongoose.Schema({
-  customerName: {type: String, required: true},
-  phoneNumber: {type: String, minlength: 10, maxlength: 13},
+  customerName: { type: String, required: true },
+  phoneNumber: { type: String, minlength: 10, maxlength: 13 },
   shop: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  email: {type:String },
+  email: { type: String },
   numberOfCars: Number,
-}, {timestamps: true}
+}, { timestamps: true }
 )
 
 module.exports = mongoose.model('Customer', exampleSchema)
->>>>>>> 803ab68689331f8895f789b3d01b7a04eeabb35f
